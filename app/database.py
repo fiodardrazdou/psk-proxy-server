54cceaa41b505cc28fa53a07000030d2d2cc9ce4
Fiodar Drazdou <dfgckpsk@protonmail.com>
2023-08-08 18:44:01 +0200

    changed database url rules

--- app/database.py
+++ app/database.py
@@ -3,6 +3,8 @@ from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 
 from config import settings
 
+
+database_url = f"postgresql+asyncpg://{settings.db_user}:{settings.db_password}@/{settings.db_name}?host={settings.db_host}"
 engine = create_async_engine(settings.database_url)
 async_session = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
 
